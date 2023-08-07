3c43753291b237af775ff4307bbb83a8e5b2a6e7
Smagin Vladimir <smagin.vovan09@gmail.com>
2023-08-07 14:18:07 +0600

    added insert contacts table

--- vendor/database.sql
+++ vendor/database.sql
@@ -9,4 +9,30 @@ CREATE TABLE IF NOT EXISTS contacts
     KEY
 (
     id
-), name TEXT NOT NULL, tel TEXT NOT NULL);
+),
+    name TEXT NOT NULL,
+    tel TEXT NOT NULL
+    );
+
+INSERT INTO contacts (name, tel)
+VALUES ('Петя', '2332323232323232');
+INSERT INTO contacts (name, tel)
+VALUES ('Вася', '3232233211324444');
+INSERT INTO contacts (name, tel)
+VALUES ('Гоша', '2332323232323232');
+INSERT INTO contacts (name, tel)
+VALUES ('Даня', '3232233211324444');
+INSERT INTO contacts (name, tel)
+VALUES ('Эрик', '2332323232323232');
+INSERT INTO contacts (name, tel)
+VALUES ('Наташа', '32323232323223322333223');
+INSERT INTO contacts (name, tel)
+VALUES ('Настя', '2332323232323232');
+INSERT INTO contacts (name, tel)
+VALUES ('Миша', '32323232323223322333223');
+INSERT INTO contacts (name, tel)
+VALUES ('Костя', '2332323232323232');
+INSERT INTO contacts (name, tel)
+VALUES ('Бублик', '32323232323223322333223');
+INSERT INTO contacts (name, tel)
+VALUES ('Евгений', '22255544849494994');
